f31093ab03573e157c8d62f26e57fceeee8c639c
hualde <javhualde@gmail.com>
2024-10-28 20:04:54 +0100

    d0

--- modules/image_processor.py
+++ modules/image_processor.py
@@ -5,7 +5,7 @@ from PIL import Image, ImageOps, ImageFilter, ImageEnhance
 class ImageProcessor:
     @staticmethod
     def apply_effects(image):
-        original = Image.open(image)
+        original = Image.open(image).convert('RGB')
         greyscale = ImageOps.grayscale(original)
         blur = original.filter(ImageFilter.GaussianBlur(radius=5))
         contrast = ImageEnhance.Contrast(original).enhance(1.5)
@@ -20,5 +20,7 @@ class ImageProcessor:
     @staticmethod
     def image_to_base64(image):
         buffered = BytesIO()
+        if image.mode == 'RGBA':
+            image = image.convert('RGB')
         image.save(buffered, format="JPEG")
         return base64.b64encode(buffered.getvalue()).decode('utf-8')
